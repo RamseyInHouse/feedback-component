c38f149b0e58db14b532179855d506becf559eef
Alex MacArthur <alex.macarthur@ramseysolutions.com>
2021-03-02 15:14:32 -0600

    remove jest, use cypress

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -1,21 +1,18 @@
-const getShadow = (id) => {
-    return cy.get(`feedback-block#${id}`).shadow();
+const getComponent = (id) => {
+    return cy.get(`feedback-block#${id}`);
 }
 
 beforeEach(() => {
-    cy.visit('http://localhost:5000')
+    cy.visit('/')
 });
 
 describe("rendering", () => {
     it('shows default markup', () => {
-        getShadow('default')
+        getComponent('default')
             .find('.FeedbackBlock-ctaText')
-            .should(e => {
-                const [ctaText] = e.get();
-                expect(ctaText.textContent).to.contains('Was this helpful?');
-            });
+            .contains('Was this helpful?')
 
-        getShadow('default')
+        getComponent('default')
             .find('.FeedbackBlock-button')
             .should(e => {
                 expect(e.first().data('feedback-block-value')).to.equal(1);
@@ -24,12 +21,22 @@ describe("rendering", () => {
     });
 
     it('respects custom slots', () => {
-        getShadow('custom-slots')
+        getComponent('custom-slots')
             .find('.FeedbackBlock-ctaText')
             .should(e => {
                 const [ctaText] = e.get();
                 const [renderedSlot] = ctaText.querySelector('[name="cta"]').assignedNodes({ flatten: true });
-                expect(renderedSlot.textContent).to.contains('Custom CTA!'); 
+
+                expect(renderedSlot.textContent).to.contains('Custom CTA!');
+            });
+
+        getComponent('custom-slots')
+            .find('.FeedbackBlock-confirmationMessage')
+            .should(e => {
+                const [ctaText] = e.get();
+                const [renderedSlot] = ctaText.querySelector('[name="confirmation"]').assignedNodes({ flatten: true });
+
+                expect(renderedSlot.textContent).to.contains('Custom thank you!');
             });
     });
 });
@@ -43,7 +50,7 @@ describe("event firing", () => {
                 });
             });
 
-            getShadow('default')
+            getComponent('default')
                 .find('.FeedbackBlock-button')
                 .last()
                 .click();
@@ -60,7 +67,7 @@ describe("event firing", () => {
                 });
             });
 
-            getShadow('default')
+            getComponent('default')
                 .find('.FeedbackBlock-button')
                 .first()
                 .click();
@@ -68,5 +75,55 @@ describe("event firing", () => {
 
         expect(result.value).to.equal(1);
     });
+
+    it("sends base data in event payload", async () => {
+        const result = await new Cypress.Promise((resolve) => {
+            cy.document().then((doc) => {
+                doc.addEventListener('feedback:interaction', function (e) {
+                    return resolve(e.detail);
+                });
+            });
+
+            getComponent('default')
+                .find('.FeedbackBlock-button')
+                .first()
+                .click();
+        });
+
+        expect(result).to.have.property('instance');
+        expect(result).to.have.property('value');
+        expect(result.options).to.deep.eq([0, 1]);
+    });
+
+    it("sends data attribute value in event payload", async () => {
+        const result = await new Cypress.Promise((resolve) => {
+            cy.document().then((doc) => {
+                doc.addEventListener('feedback:interaction', function (e) {
+                    return resolve(e.detail);
+                });
+            });
+
+            getComponent('custom-data')
+                .find('.FeedbackBlock-button')
+                .first()
+                .click();
+        });
+
+        expect(result).to.have.property('something', 'some value!')
+        expect(result).to.have.property('somethingElse', 'some other value!')
+    });
 });
 
+describe("UI changes upon interaction", () => {
+    it("shows confirmation message after being clicked", () => {
+        getComponent('default')
+            .find('.FeedbackBlock-button')
+            .last()
+            .click();
+
+        getComponent('default')
+            .find('.FeedbackBlock-confirmationContent')
+            .should('have.css', 'z-index', '1')
+            .and('have.css', 'opacity', '1')
+    });
+});
--- src/index.test.js
+++ src/index.test.js
@@ -12,39 +12,39 @@ describe("FeedbackComponent", () => {
     window.document.body.innerHTML = `<feedback-block data-element-id="my-element-id"></feedback-block>`;
   });
 
-  it("shows confirmation message after interaction", async () => {
-    const thumbsUpButton = getByLabelText(getShadowRoot(), "thumbs up");
-    const confirmationContainer = getShadowRoot().querySelector(
-      "[data-feedback-block-confirmation]"
-    );
+  // it("shows confirmation message after interaction", async () => {
+  //   const thumbsUpButton = getByLabelText(getShadowRoot(), "thumbs up");
+  //   const confirmationContainer = getShadowRoot().querySelector(
+  //     "[data-feedback-block-confirmation]"
+  //   );
 
-    thumbsUpButton.click();
+  //   thumbsUpButton.click();
 
-    process.nextTick(() => {
-      expect(confirmationContainer).toHaveStyle({
-        zIndex: "1",
-        opacity: "1",
-      });
-    });
-  });
+  //   process.nextTick(() => {
+  //     expect(confirmationContainer).toHaveStyle({
+  //       zIndex: "1",
+  //       opacity: "1",
+  //     });
+  //   });
+  // });
 
-  it("fires feedback:interaction event with data set as attributes", async () => {
-    const thumbsUpButton = getByLabelText(getShadowRoot(), "thumbs up");
-    await new Promise((resolve) => {
-      const callback = (e) => {
-        expect(e.detail).toMatchObject({
-          elementId: "my-element-id",
-          options: [0, 1],
-          value: 1,
-        });
+  // it("fires feedback:interaction event with data set as attributes", async () => {
+  //   const thumbsUpButton = getByLabelText(getShadowRoot(), "thumbs up");
+  //   await new Promise((resolve) => {
+  //     const callback = (e) => {
+  //       expect(e.detail).toMatchObject({
+  //         elementId: "my-element-id",
+  //         options: [0, 1],
+  //         value: 1,
+  //       });
 
-        removeEventListener("feedback:interaction", callback);
-        resolve();
-      };
+  //       removeEventListener("feedback:interaction", callback);
+  //       resolve();
+  //     };
 
-      addEventListener("feedback:interaction", callback);
+  //     addEventListener("feedback:interaction", callback);
 
-      thumbsUpButton.click();
-    });
-  });
+  //     thumbsUpButton.click();
+  //   });
+  // });
 });
